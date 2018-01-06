623fb73eae3035412faa369e8493bcadfd7163a5
Mikolaj-Walkowiak <33766092+Mikolaj-Walkowiak@users.noreply.github.com>
2018-01-06 17:56:01 +0100

    Update RAM.hpp

--- RAM.hpp
+++ RAM.hpp
@@ -12,24 +12,25 @@ class RAM {
 	std::vector<Block> freeBlocks; //lista zajetych blokow
 	std::vector<Block> claimedBlocks; //lista zajetych blokow(potrzebne to komu???)
 	std::vector<writtenBlock> writtenBlocks;//bloki pamieci
-	bool isFreeBlock(int a); // Sprawdza dostêpnš pamiêæ (blok)
+	bool isFreeBlock(int a); // Sprawdza dostÄ™pnÄ… pamiÄ™Ä‡ (blok)
 	void FBRemove(PCB*a);//usuniecie wolnego bloku
 	void memWrite(PCB* a, std::string polecenie);//Zapisywanie w pamieci
-	void RAM::WriteToRam(std::string a, writtenBlock &writtenTo);//wpisywanie do ramu
+	void RAM::WriteToRam(std::string a, writtenBlock &writtenTo, int localisation);//wpisywanie do ramu
 	void memMerge();//laczy 2 wolne bloki w 1 jezeli sa obok siebie
 public:
 	RAM(); // konstruktor
-
+	
 	void addToMem(PCB*a, std::string polecenie); //dodaj do pamieci
 	void deleteFromMem(PCB*a); //usun z pamieci
 	void showRam();//wypisz cala zawartosc
 	void showRange(int start, int lenght);
 	std::string showProcess(int base);
-	void saveToRam(PCB*a, int localisation, std::string value);// wpisuje wartosc poczawszy od komorki podanej przez assembler
+	void saveToRam(int a, int localisation, std::string value);// wpisuje wartosc poczawszy od komorki podanej przez assembler
 
 
 
 															   ///////////// DO WYJEBANIA //////////////////////
 	void printBLOCKS();
 };
+
 extern RAM ram;
